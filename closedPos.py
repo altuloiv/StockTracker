c17d3bc3c77404639074646b5deaebf305d1fd37
Albert Tulo <atuloiv@gmail.com>
2022-02-25 15:36:42 -0600

    Updated SQL Tables
    Created 2 separate database making python files, closedPos.py and openPos.py. These just help make everything clean and simple also help to soften the load on separating the data from one to the other.

---
+++ closedPos.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+con = sqlite3.connect('closedPositions.db')
+cc = con.cursor()
+
+cc.execute('''
+        CREATE TABLE IF NOT EXISTS closedPositions
+        (
+        [stock_ticker] TEXT NOT NULL,
+        [entryType] TEXT NOT NULL,
+        [pos_quan] INTEGER NOT NULL,
+        [entryPrice] REAL NOT NULL,
+        [close_quan] INTEGER NOT NULL,
+        [close_price] REAL NOT NULL,
+        [outcome] REAL NOT NULL
+        )
+        ''')
+con.commit()
--- openPos.py
+++ openPos.py
@@ -1,11 +1,12 @@
 import sqlite3
 
-conn = sqlite3.connect('stocks.db')
+conn = sqlite3.connect('openPositions.db')
 c = conn.cursor()
 
 c.execute('''
-        CREATE TABLE IF NOT EXISTS stocks
+        CREATE TABLE IF NOT EXISTS openPositions
         (
+        [S_id] INTEGER PRIMARY KEY AUTOINCREMENT,
         [stock_ticker] TEXT NOT NULL,
         [entryType] TEXT NOT NULL,
         [pos_quan] INTEGER NOT NULL,
